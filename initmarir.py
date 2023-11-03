e176dc84efdac4ef7c7d1124fbbdd1692ab90ecc
danckard <deckard.the.gamer@gmail.com>
2023-11-03 13:54:42 -0400

    modified:   initmarir.py

--- initmarir.py
+++ initmarir.py
@@ -35,7 +35,15 @@ def processpdf(filename):
                 
     referencespdf=set(referencespdf)
     return referencespdf
+<<<<<<< HEAD
 
+=======
+    # .title()s
+    # testing line 
+
+
+    
+>>>>>>> 27bfaee (	modified:   initmarir.py)
 def processxls(filename):
     referencesexcel=[]
     wb_obj = openpyxl.load_workbook(filename)
